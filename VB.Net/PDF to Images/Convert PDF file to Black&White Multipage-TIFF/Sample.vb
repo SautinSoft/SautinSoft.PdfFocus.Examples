f297d11b592df4ccdf95f4ae3f7dc165510df9d6
oliver <oliver@sautinsoft.com>
2023-04-24 15:34:00 +0400

    New Code Samples

--- VB.Net/PDF to Images/Convert PDF file to Black&White Multipage-TIFF/Sample.vb
+++ VB.Net/PDF to Images/Convert PDF file to Black&White Multipage-TIFF/Sample.vb
@@ -21,7 +21,9 @@ Module Sample
             f.ImageOptions.Dpi = 200
             f.ImageOptions.ColorDepth = SautinSoft.PdfFocus.CImageOptions.eColorDepth.BlackWhite1bpp
             ' EncoderValue.CompressionCCITT4 - also makes image black&white 1 bit
-            If f.ToMultipageTiff(tiffPath, EncoderValue.CompressionCCITT4) = 0 Then
+            f.ImageOptions.TIFFCompressionType = PdfFocus.eTIFFCompressionType.CCITTFAX4
+
+            If f.ToMultipageTiff(tiffPath) = 0 Then
                 System.Diagnostics.Process.Start(New System.Diagnostics.ProcessStartInfo(tiffPath) With {.UseShellExecute = True})
             End If
         End If
